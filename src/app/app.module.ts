9456cd7a59814384e87497085b3aa22ad6e12557
MarcoSuess <81395283+MarcoSuess@users.noreply.github.com>
2021-09-01 16:19:28 +0200

    working on home-section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeSectionComponent } from './home-section/home-section.component';
+import { HomeTextComponent } from './home-text/home-text.component';
+import { HomeImageComponent } from './home-image/home-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeSectionComponent
+    HomeSectionComponent,
+    HomeTextComponent,
+    HomeImageComponent
   ],
   imports: [
     BrowserModule,
