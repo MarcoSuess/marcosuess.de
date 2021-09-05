0b47460e221b3f2d9a46f62a61b0f8f107cae3fd
MarcoSuess <81395283+MarcoSuess@users.noreply.github.com>
2021-09-05 18:43:08 +0200

    finished structure home section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeSectionComponent } from './home-section/home-section.component';
 import { HomeTextComponent } from './home-text/home-text.component';
 import { HomeImageComponent } from './home-image/home-image.component';
+import { HomeContactComponent } from './home-contact/home-contact.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { HomeImageComponent } from './home-image/home-image.component';
     NavBarComponent,
     HomeSectionComponent,
     HomeTextComponent,
-    HomeImageComponent
+    HomeImageComponent,
+    HomeContactComponent,
+    SkillsSectionComponent
   ],
   imports: [
     BrowserModule,
