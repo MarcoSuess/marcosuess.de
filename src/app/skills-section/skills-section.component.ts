bcc1f897ec3cf3a787cd5eeccc59e829b0280c99
MarcoSuess <81395283+MarcoSuess@users.noreply.github.com>
2021-09-05 21:51:52 +0200

    my skills section

--- src/app/skills-section/skills-section.component.ts
+++ src/app/skills-section/skills-section.component.ts
@@ -3,13 +3,54 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skills-section',
   templateUrl: './skills-section.component.html',
-  styleUrls: ['./skills-section.component.scss']
+  styleUrls: ['./skills-section.component.scss'],
 })
+
+
+
 export class SkillsSectionComponent implements OnInit {
+  skills = [
+    {
+      name: 'JavaScript',
+      img: 'assets/skills_icon/javascript-icon.png',
+    },
+    {
+      name: 'HTML',
+      img: 'assets/skills_icon/html-icon.png',
+    },
+    {
+      name: 'CSS',
+      img: 'assets/skills_icon/css-icon.png',
+    },
+    {
+      name: 'Angular',
+      img: 'assets/skills_icon/angular-icon.png',
+    },
+    {
+      name: 'Git',
+      img: 'assets/skills_icon/git-icon.png',
+    },
+    {
+      name: 'SCRUM',
+      img: 'assets/skills_icon/scrum-icon.png',
+    },
+    {
+      name: 'Rest API',
+      img: 'assets/skills_icon/api-icon.png',
+    },
+    {
+      name: 'Firebase Database',
+      img: 'assets/skills_icon/database-icon.png',
+    }
+  ];
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+
+  constructor() {
+  
   }
 
+  ngOnInit(): void {}
 }
+ 
