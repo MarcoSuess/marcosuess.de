fc92a0437a27f3a831ccb4d0f45ac64bc6244c16
MarcoSuess <81395283+MarcoSuess@users.noreply.github.com>
2021-09-06 16:09:57 +0200

    filter function

--- src/app/work-section/work-section.component.ts
+++ src/app/work-section/work-section.component.ts
@@ -3,18 +3,66 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-work-section',
   templateUrl: './work-section.component.html',
-  styleUrls: ['./work-section.component.scss']
+  styleUrls: ['./work-section.component.scss'],
 })
 export class WorkSectionComponent implements OnInit {
+  projects = [
+    {
+      name: 'Ring of Fire',
+      description: 'test',
+      category: 'Angular',
+    },
+    {
+      name: 'Sharkie',
+      description: 'tesafdf',
+      category: 'Java Script',
+    },
+  ];
 
-  constructor() { }
+  titles = [
+    {
+      name: 'All',
+      active: true,
+    },
+    {
+      name: 'Angular',
+      active: false,
+    },
+    {
+      name: 'Java Script',
+      active: false,
+    },
+  ];
 
-  ngOnInit(): void {
+  showProjects: any;
+
+  assort(title: string) {
+    for (let i = 0; i < this.titles.length; i++) {
+      let category = this.titles[i].name;
+
+      if (category == title) {
+        this.titles[i].active = true;
+        this.filterCategory(title);
+      } else {
+        this.titles[i].active = false;
+      }
+    }
   }
 
+  filterCategory(title: string) {
+    this.showProjects = this.projects.filter(
+      (project) => project.category == title
+    );
 
-  test() {
-    console.log('test');
-    
+    if (this.showProjects.length == 0) {
+      this.showProjects = this.projects;
+    }
+    console.log(this.showProjects);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.showProjects = this.projects;
   }
 }
