66e8a41220f6bbd82fde501caf6111f4938e4b2f
MarcoSuess <81395283+MarcoSuess@users.noreply.github.com>
2021-12-08 21:59:32 +0100

    little design fixes

--- src/app/about-section/about-section.component.ts
+++ src/app/about-section/about-section.component.ts
@@ -8,22 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class AboutSectionComponent implements OnInit {
   stories = [
     {
-      title: '#1 Start in the Passion',
+      title: 'Start in the Passion',
       description: 'after completing my training, i was able to develop my own system with excel and have found that i am very happy when others benefit from it.',
       icon: 'assets/story_icons/walk-line.png',
     },
     {
-      title: '#My journey of development',
-      description: 'During the time when I was unemployed, I thought a lot about my professional life, I started with c ++ udemy courses, but I noticed that it hadn t brought me any further. In 2021 I decided to go to the Developer Academy and was able to develop my entire passion through it.',
+      title: 'My journey of development',
+      description: 'I started with C++ Udemy courses, but I noticed that it hadn t brought me any further. In 2021 I decided to go to the Developer Academy and was able to develop my entire passion through it.',
       icon: 'assets/story_icons/heart-line.png',
     },
     {
-      title: '3# Project experience',
-      description: 'You can find everything from simple landing pages to complex web applications in my portfolio.',
+      title: 'Project experience',
+      description: 'You can find  complex web applications in my portfolio.',
       icon: 'assets/story_icons/search-line.png',
     },
     {
-      title: '4# The next Challenge',
+      title: 'The next Challenge',
       description: 'Im looking forward to contribute my knowledge of Javascript and Angular to your next project.',
       icon: 'assets/story_icons/flight-takeoff-line.png',
     },
